2e717270b6ac2d5f912b88da9d76fa89b7266704
root <root@sbhost.asia-east1-b.c.feisty-outrider-270809.internal>
2021-02-09 10:56:11 +0800

    ajax

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 	define('TITLE','宋斌的网络日志');
-	define('DIR','../jdzcn.github.io/_posts/');
+	define('DIR','_posts/');
 ?>
--- listsubdir.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dir = new DirectoryIterator('.');
-foreach ($dir as $fileinfo) {
-    if ($fileinfo->isDir() && !$fileinfo->isDot()) {
-        echo $fileinfo->getFilename().'<br>';
-    }
-}
-?>
