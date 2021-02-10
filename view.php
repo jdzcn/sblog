d1c6b5d31b3d1c3dddfdc9068f6e92ff8a8d86b1
root <root@sbhost.asia-east1-b.c.feisty-outrider-270809.internal>
2021-02-10 20:00:50 +0800

    up

--- view.php
+++ view.php
@@ -6,8 +6,8 @@ include('Parser.php');
 $filename=$_GET['name'];
 $content = file_get_contents($filename);
 $lines = explode("\n", $content);
-echo '<h3>'.substr($lines[2],7).'</h3><br>';
-echo substr($filename,strlen(DIR),10);
+echo '<h3>'.substr($lines[2],7).'</h3>';
+echo "<p>".substr($filename,strlen(DIR),10)."</p>";
 $skipped_content = implode("\n", array_slice($lines, 5));
 
 $parser = new HyperDown\Parser;
